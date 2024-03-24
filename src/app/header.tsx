e170a0933a144304349935f05025689f6950d19a
md-anas-sabah <mdanassabah@gmail.com>
2024-03-24 21:34:55 +0530

    finish setting up next-auth

---
+++ src/app/header.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export function Header() {
+  const session = useSession();
+  return (
+    <div>
+      {session.data ? (
+        <Button onClick={() => signOut()}>Sign Out</Button>
+      ) : (
+        <Button onClick={() => signIn("google")}>Sign In</Button>
+      )}
+      <ModeToggle />
+    </div>
+  );
+}
